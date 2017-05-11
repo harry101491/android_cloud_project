06250813d45f4b13d77426e04644a0cb0e212e6d
ankeetghosh <ag5347@nyu.edu>
2017-05-11 12:43:16 -0400

    error checks added

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var elasticSearch = new elasticsearch({
 });
 
 var config = {
-  projectId: 'finalproj-167105',
-  keyFilename: 'finalProj-3d8fb5b0c6f8.json'
+  projectId: 'Enter your project ID',
+  keyFilename: 'Enter the path of your key file'
 };
 
 var datastore = require('@google-cloud/datastore')(config);
@@ -41,8 +41,15 @@ app.get('/restaurants/zip/:id', function(req, res) {
         //size: 1
     }
 }, function(err, data) {
-	console.log(data.hits.hits);
-	res.send(data.hits.hits);
+	if(err !== null)
+	{
+		console.log(err);
+	}
+	else
+	{
+		console.log(data.hits.hits);
+		res.send(data.hits.hits);
+	}
  });
 });
 
@@ -60,42 +67,64 @@ app.get('/social/zip/:id', function(req, res) {
         //size: 1
     }
 }, function(err, data) {
-
-	console.log(data.hits.hits);
-	res.send(data.hits.hits);
+	if(err !== null)
+	{
+		console.log(err);
+	}
+	else
+	{
+		console.log(data.hits.hits);
+		res.send(data.hits.hits);
+	}
  });
 });
 
 app.get('/complaints/zip/:id', function(req, res) {
     store.get(req.params.id, function(err, data) {
-	console.log(data);
-	res.send(data);
+	if(err !== null)
+	{
+		console.log(err);
+	}
+	else
+	{
+		console.log(data);
+		res.send(data);
+	}
+
     });
 });
 
 app.get('/rating/zip/:id', function(req, res) {
 	store.get(req.params.id, function(err, data) {
-	var val = data;
-	var rate = val[val.length-1];
-	//console.log(rate);
-	if(rate >= 71 && rate <=100){
-		var text = "It looks like the law and order is very strict in this area. Almost all the complaints that were made to the police has been taken care of. We can assume that the neighborhood is a good neighborhood";
-		var PeaceLevel= "High";
-		console.log({ "PeaceLevel":PeaceLevel, "text":text });
-		res.send({"PeaceLevel":PeaceLevel, "text":text });
+	if(err !== null)
+	{
+		console.log(err);
 	}
-	else if(rate >= 31 && rate <=70){
-		var text = "It looks like the law and order is average in this area. There are still a lot of complaints that are yet to be taken care of, by the police. We can assume that the people are slightly overwhelmed by the crimes. This is what an average neighborhood looks like. People are happy with their own near and dear ones with little interaction with the outside world. And they are also very much socially aware of their surroundings to avoid harmful situations";
-		var PeaceLevel= "Medium";
-		res.send({"PeaceLevel":PeaceLevel, "text":text });
-	}
-	else if(rate >= 0 && rate <=30){
-		var text = "It looks like the law and order is not very strict in this area. Almost all the complaints that were made to the police are open. The police is overwhelmed by the amount of complaints related to crime and social life. Safety is a major concern in these areas. And by this time, people have recognized that it is their responsibility to take care of themselves. Being Self independent and highly alert is a requirement to survive here. We can assume that the neighborhood is a very bad one.";
-		var PeaceLevel= "Low";
-		res.send({"PeaceLevel":PeaceLevel, "text":text });
+	else
+	{
+		var val = data;
+		var rate = val[val.length-1];
+		//console.log(rate);
+		if(rate >= 71 && rate <=100){
+			var text = "It looks like the law and order is very strict in this area. Almost all the complaints that were made to the police has been taken care of. We can assume that the neighborhood is a good neighborhood";
+			var PeaceLevel= "High";
+			console.log({ "PeaceLevel":PeaceLevel, "text":text });
+			res.send({"PeaceLevel":PeaceLevel, "text":text });
+		}
+		else if(rate >= 31 && rate <=70){
+			var text = "It looks like the law and order is average in this area. There are still a lot of complaints that are yet to be taken care of, by the police. We can assume that the people are slightly overwhelmed by the crimes. This is what an average neighborhood looks like. People are happy with their own near and dear ones with little interaction with the outside world. And they are also very much socially aware of their surroundings to avoid harmful situations";
+			var PeaceLevel= "Medium";
+			res.send({"PeaceLevel":PeaceLevel, "text":text });
+		}
+		else if(rate >= 0 && rate <=30){
+			var text = "It looks like the law and order is not very strict in this area. Almost all the complaints that were made to the police are open. The police is overwhelmed by the amount of complaints related to crime and social life. Safety is a major concern in these areas. And by this time, people have recognized that it is their responsibility to take care of themselves. Being Self independent and highly alert is a requirement to survive here. We can assume that the neighborhood is a very bad one.";
+			var PeaceLevel= "Low";
+			res.send({"PeaceLevel":PeaceLevel, "text":text });
+		}
 	}
     });
 });
+
 
 
 app.listen(3000);
